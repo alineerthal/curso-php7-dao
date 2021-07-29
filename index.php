a6ef35ce07e9e12a080eb7db40550df8631db87e
Aline Erthal <aerp.14@gmail.com>
2021-07-29 15:25:18 -0300

    implementamos um pouco mais a classe Usuario

--- index.php
+++ index.php
@@ -2,16 +2,42 @@
 
 require_once("config.php");
 
-//$sql = new Sql();
+/*
+$sql = new Sql();
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-//echo json_encode($usuarios);
+echo json_encode($usuarios);
+*/
 
+//Carrega um usuário:
+/*
 $root = new Usuario();
 
 $root->loadbyId(3);
 
 echo $root;
+*/
+
+//Carrega uma lista de usuários:
+/*
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+//Carrega uma lista de usuarios, buscando pelo login:
+/*
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+*/
+
+//Carrega um usuário, usando o login e a senha:
+$usuario = new Usuario();
+
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 
 ?>
